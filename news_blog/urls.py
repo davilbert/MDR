a6834f7e2015b0f713887008a9522a5bb24be7c4
davilbert <33199441+davilbert@users.noreply.github.com>
2018-03-05 14:17:37 +0600

    Update urls.py

--- news_blog/urls.py
+++ news_blog/urls.py
@@ -4,10 +4,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:article_id>/', views.get_article, name='article'),
     path('about/', views.about_author, name='author'),
-    path('new/', views.new_article, name='new_article'),
+    path('<int:article_id>new/', views.new_article, name='new_article'),
     path('<int:article_id>/edit', views.edit_article, name='edit'),
     path('past/', views.past_time, name='past'),
-    path('now/', views.now_life, name='now'),
+    path('<int:article_id>now/', views.now_life, name='now'),
 ]
 
 
